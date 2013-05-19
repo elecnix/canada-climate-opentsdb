eb5fff5ff414da9b663e8e67f912cea6104af00f
Sergey Scherbina <sergey.scherbina@gmail.com>
2013-05-19 20:42:33 +0300

    scala-csv

--- project/ClimatBuild.scala
+++ project/ClimatBuild.scala
@@ -12,6 +12,7 @@ object ClimatBuild extends Build {
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.9.2",
       // add other settings here
+      libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "0.8.0",
       libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test"
     )
   )
